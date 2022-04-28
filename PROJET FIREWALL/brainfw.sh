b54f3a798b1abdc5e653dccca86c02fd48d6d199
Jerome Aguas <90044654+jeyinked@users.noreply.github.com>
2022-04-28 13:05:08 +0200

    Update brainfw.sh

--- PROJET FIREWALL/brainfw.sh
+++ PROJET FIREWALL/brainfw.sh
@@ -6,9 +6,33 @@
 # {blockformat} -> ip add, iprange ou pays 
 # {target} -> 192.168.x.x, 192.168.x.x/24 or DE/FR
 
-############################################################################################################################################################################
+####################################################################[VARIABLES]##################################################################################
 
 WOKPLACE="/root/temp"
 ACTION="$1"
 BLOCKFORMAT="$2"
 TARGET="$3"
+
+function check_workplace()
+{
+}
+
+function get_country()
+{
+}
+
+function blockip()
+{
+}
+
+function unblockip()
+{
+}
+
+function core()
+{
+}
+
+function action()
+{
+}
