f44279f8e1e8973263dea5e8eff97af80b5ab22a
Jerome Aguas <90044654+jeyinked@users.noreply.github.com>
2022-04-28 12:58:38 +0200

    Update brainfw.sh

--- PROJET FIREWALL/brainfw.sh
+++ PROJET FIREWALL/brainfw.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 # ./brainfw.sh {action} [blockformat} {target}
+
+# {action} -> bloque ou autorise 
+# {blockformat} -> ip add, iprange ou pays 
+# {target} -> 192.168.x.x, 192.168.x.x/24 or DE/FR
+
+############################################################################################################################################################################
+
+WOKPLACE="/root/temp"
+ACTION="$1"
+BLOCKFORMAT="$2"
+TARGET="$3"
